9e49ceafb1963173689b0fc481d09dec6b7f480e
lizhnayi <825921471@qq.com>
2019-11-06 08:58:20 +0800

    日考提交

---
+++ rikaosrc/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+{{forEach(arr)}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+            arr:[1,2,3,4]
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+        forEach(item){
+            console.log(item[0]);
+            console.log(item[1]);
+            console.log(item[2]);
+            console.log(item[3]);
+        }
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
